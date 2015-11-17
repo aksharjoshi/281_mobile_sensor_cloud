0433c2c26c2aca54fd83074dddb67a6ce544cce6
Akshar Joshi <akshar.joshi91@gmail.com>
2015-11-16 22:56:07 -0800

    First commit with java instance invokation

---
+++ 281_project/281_invoke_instance/src/DBInsert.java
@@ -0,0 +1,116 @@
+
+public class DBInsert {
+
+	String InstanceId, OwnerId, ImageId, KeyName, InstanceType, VpcId, Status, PublicIpAddress, PrivateIpAddress, GroupId, GroupName;
+	
+	
+	public String getInstanceId() {
+		return InstanceId;
+	}
+
+
+	public void setInstanceId(String instanceId) {
+		InstanceId = instanceId;
+	}
+
+	
+	public String getOwnerId() {
+		return OwnerId;
+	}
+
+	
+	public void setOwnerId(String ownerId) {
+		OwnerId = ownerId;
+	}
+
+
+	public String getImageId() {
+		return ImageId;
+	}
+
+
+	public void setImageId(String imageId) {
+		ImageId = imageId;
+	}
+
+
+	public String getKeyName() {
+		return KeyName;
+	}
+
+
+	public void setKeyName(String keyName) {
+		KeyName = keyName;
+	}
+
+
+	public String getInstanceType() {
+		return InstanceType;
+	}
+
+
+	public void setInstanceType(String instanceType) {
+		InstanceType = instanceType;
+	}
+
+
+	public String getVpcId() {
+		return VpcId;
+	}
+
+
+	public void setVpcId(String vpcId) {
+		VpcId = vpcId;
+	}
+
+
+	public String getStatus() {
+		return Status;
+	}
+
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+
+	public String getPublicIpAddress() {
+		return PublicIpAddress;
+	}
+
+
+	public void setPublicIpAddress(String publicIpAddress) {
+		PublicIpAddress = publicIpAddress;
+	}
+
+
+	public String getPrivateIpAddress() {
+		return PrivateIpAddress;
+	}
+
+
+	public void setPrivateIpAddress(String privateIpAddress) {
+		PrivateIpAddress = privateIpAddress;
+	}
+
+
+	public String getGroupId() {
+		return GroupId;
+	}
+
+
+	public void setGroupId(String groupId) {
+		GroupId = groupId;
+	}
+
+
+	public String getGroupName() {
+		return GroupName;
+	}
+
+
+	public void setGroupName(String groupName) {
+		GroupName = groupName;
+	}
+
+}
